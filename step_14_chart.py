f58354ea99daee7f7190580ae745bc64c4f8f378
salvekajal776-gif <salvekajal776@gmail.com>
2026-07-15 22:40:23 +0530

    Add files via upload

---
+++ step_14_chart.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Chart Example")
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["A", "B", "C"]
+)
+
+st.line_chart(chart_data)
